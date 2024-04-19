d02700a39c6c9bb38811af820fbc1481c119fc47
krsntn <kanahan0110@gmail.com>
2024-04-20 02:38:32 +0800

    update config

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
+  { "folke/noice.nvim", enabled = false },
   -- { "ggandor/leap.nvim", enabled = false },
   -- { "ggandor/flit.nvim", enabled = false },
   -- { "nvim-pack/nvim-spectre", enabled = false },
