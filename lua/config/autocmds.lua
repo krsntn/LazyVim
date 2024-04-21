4187ca8a228b7cb67996fea01dbbd6e52598b61d
krsntn <kanahan0110@gmail.com>
2024-04-21 13:37:39 +0800

    show telescope oldfiles once enter nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+if vim.fn.argv(0) == "" then
+  require("telescope.builtin").oldfiles({ cwd_only = true })
+end
